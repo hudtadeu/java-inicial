02afe7d062a296beb4f867dbf265499e3e92d326
Hudson Tadeu <hudinvesting@gmail.com>
2022-06-29 12:31:59 -0300

    Adicionando projeto de configuração de visibilidade

--- AulaDePoo/src/main/java/auladepoo/AulaDePoo.java
+++ AulaDePoo/src/main/java/auladepoo/AulaDePoo.java
@@ -8,6 +8,13 @@ public class AulaDePoo {
         c1.cor = "Azul";
         c1.ponta = 0.5f;
         c1.tampar();
+        c1.rabiscar();
         c1.status();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Big";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.rabiscar();
     }
 }
